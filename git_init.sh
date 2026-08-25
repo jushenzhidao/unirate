868985d5e74221c0d29c71e5a4504d824caede0a
Betterme <313303303@qq.com>
2026-08-25 13:59:32 +0800

    init

--- git_init.sh
+++ git_init.sh
@@ -7,7 +7,7 @@ git commit -m "init"
 #git rm  -r dist
 #git remote add origin git@github.com:yuanjie-ai/MeUtils.git
 #git branch -M master
-git push -u origin master -f
+git push -u origin main -f
 # git remote remove origin
 
 
